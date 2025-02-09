795dc7123e38cc1fa6794e5a646b8b3ee6b5fc04
Grzegorz Cagara <grzegorzcagara@gmail.com>
2025-02-09 13:43:11 +0100

    *extend contact section

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
+import { Github, Linkedin, Mail } from 'lucide-react'
 
 export const Contact: React.FC = () => {
     return (
-        <div id="contact" className="navSection section wb">
+        <div id="contact" className="navSection section lb">
             <Container>
                 <Row>
                     <h3>Contact</h3>
-                    <p>
-                        Email: grzgorzcagara@gmail.com
-                        <br />
-                        LI:
-                        <a href="https://www.linkedin.com/in/grzegorz-cagara/">
+                    <div>
+                        <Mail />&nbsp;grzgorzcagara@gmail.com
+                    </div>
+                    <div>
+                        <Linkedin />&nbsp;
+                        <a href="https://www.linkedin.com/in/grzegorz-cagara/" style={{textDecoration: 'none'}}>
                             https://www.linkedin.com/in/grzegorz-cagara/
                         </a>
-                    </p>
+                    </div>
+                    <div>
+                        <Github />&nbsp;
+                        <a href="https://github.com/cagaragrzegorz" style={{textDecoration: 'none'}}>
+                            https://github.com/cagaragrzegorz
+                        </a>
+                    </div>
                 </Row>
             </Container>
         </div>
